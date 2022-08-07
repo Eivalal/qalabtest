c488e44926277419f93af898cb8d3c9ba1b4d084
Eivalal <wuilmer.ivala@gmail.com>
2022-08-07 17:46:00 -0500

    Desde pc

---
+++ Ejercicio1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nomb="Enrique";
+        String nomb2="Juan";
+        String nomb3="Luis";
+
+
+        System.out.println("Users: "+nomb+", "+nomb2+", "+nomb3);
+    }
+}
